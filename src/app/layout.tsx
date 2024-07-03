f67405db2334fe58b109f7993986d0cda15c6be8
k2pbac <krisbachan123@hotmail.com>
2024-07-03 09:04:01 -0400

    base files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import "./main.scss";
-import Navbar from "@/components/navbar";
+import Navbar from "@/components/navbar/navbar";
 
 const dm = DM_Sans({ subsets: ["latin"] });
 
