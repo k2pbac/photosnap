ed2826777667d2a3f2b2832d8b270bc311c23864
k2pbac <krisbachan123@hotmail.com>
2024-07-05 12:42:24 -0400

    move css to separate file

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,3 +1,15 @@
+import Header from "@/components/header/header";
+
 export default function Pricing() {
-  return <div></div>;
+  return (
+    <div className="pricing-page">
+      <Header
+        title={"FEATURES"}
+        description={
+          "We make sure all of our features are designed to be loved by every aspiring and even professional photograpers who wanted to share their stories."
+        }
+        image={"/features/desktop/hero.jpg"}
+      />
+    </div>
+  );
 }
