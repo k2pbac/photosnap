1946ffd44adab303b37ae912068db50f42ca7d46
k2pbac <krisbachan123@hotmail.com>
2024-07-07 09:33:54 -0400

    features list on features page

--- src/app/features/page.tsx
+++ src/app/features/page.tsx
@@ -1,9 +1,14 @@
+"use client";
+
 import FeatureList from "@/components/feature/feature-list";
 import featuresData from "./features.json";
 import BetaContainer from "@/components/beta/beta-container";
 import Header from "@/components/header/header";
+import useWindowSize from "@/customWidth";
 
 export default function Features() {
+  const size = useWindowSize();
+
   return (
     <div className="features-page">
       <Header
@@ -11,7 +16,13 @@ export default function Features() {
         description={
           "We make sure all of our features are designed to be loved by every aspiring and even professional photograpers who wanted to share their stories."
         }
-        image={"/features/desktop/hero.jpg"}
+        image={`${
+          size[0] <= 540
+            ? "features/mobile/hero.jpg"
+            : size[0] <= 864
+            ? "/features/tablet/hero.jpg"
+            : "/features/desktop/hero.jpg"
+        }`}
       />
       <FeatureList data={featuresData} amount={featuresData.length} />
       <BetaContainer />
