4c272de90fa6f9d1ab4eeb0abef1b41548a130ed
k2pbac <krisbachan123@hotmail.com>
2024-07-07 06:12:05 -0400

    change footer left to grid

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -12,43 +12,43 @@ export default function Footer() {
             width={160}
             height={16}
           />
-          <div className="icons">
-            <img
-              id="facebook"
-              src="/assets/shared/desktop/facebook.svg"
-              alt="facebook-icon"
-              width={20}
-              height={20}
-            />
-            <img
-              id="youtube"
-              src="/assets/shared/desktop/youtube.svg"
-              alt="youtube-icon"
-              width={20}
-              height={20}
-            />
-            <img
-              id="twitter"
-              src="/assets/shared/desktop/twitter.svg"
-              alt="twitter-icon"
-              width={20}
-              height={20}
-            />
-            <img
-              id="pinterest"
-              src="/assets/shared/desktop/pinterest.svg"
-              alt="pinterest-icon"
-              width={20}
-              height={20}
-            />
-            <img
-              id="instagram"
-              src="/assets/shared/desktop/instagram.svg"
-              alt="instagram-icon"
-              width={20}
-              height={20}
-            />
-          </div>
+        </div>
+        <div className="icons">
+          <img
+            id="facebook"
+            src="/assets/shared/desktop/facebook.svg"
+            alt="facebook-icon"
+            width={20}
+            height={20}
+          />
+          <img
+            id="youtube"
+            src="/assets/shared/desktop/youtube.svg"
+            alt="youtube-icon"
+            width={20}
+            height={20}
+          />
+          <img
+            id="twitter"
+            src="/assets/shared/desktop/twitter.svg"
+            alt="twitter-icon"
+            width={20}
+            height={20}
+          />
+          <img
+            id="pinterest"
+            src="/assets/shared/desktop/pinterest.svg"
+            alt="pinterest-icon"
+            width={20}
+            height={20}
+          />
+          <img
+            id="instagram"
+            src="/assets/shared/desktop/instagram.svg"
+            alt="instagram-icon"
+            width={20}
+            height={20}
+          />
         </div>
         <div className="page-links">
           <ul>
