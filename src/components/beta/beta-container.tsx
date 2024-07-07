df9506c45ba1c5208a113517ccb2da3d906113ce
k2pbac <krisbachan123@hotmail.com>
2024-07-07 09:53:23 -0400

    finish features page

--- src/components/beta/beta-container.tsx
+++ src/components/beta/beta-container.tsx
@@ -1,9 +1,19 @@
+"use client";
+
+import useWindowSize from "@/customWidth";
 import Arrow from "../arrow/arrow";
 
 export default function BetaContainer() {
+  const size = useWindowSize();
+
   return (
     <div className="beta">
-      <img src="/assets/shared/desktop/bg-beta.jpg" alt="beta-background" />
+      <img
+        src={`/assets/shared/${
+          size[0] <= 580 ? "mobile" : size[0] <= 864 ? "tablet" : "desktop"
+        }/bg-beta.jpg`}
+        alt="beta-background"
+      />
       <div className="beta-text">
         <h2>{"We’re in beta. Get your invite today!".toUpperCase()}</h2>
         <Arrow
