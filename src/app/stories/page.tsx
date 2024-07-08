f9a2c80eeb5464d396c7837ba7b3760cb6bb0825
k2pbac <krisbachan123@hotmail.com>
2024-07-08 16:14:17 -0400

    finish stories page

--- src/app/stories/page.tsx
+++ src/app/stories/page.tsx
@@ -1,10 +1,14 @@
+"use client";
+
 import StoryCardList from "@/components/story-card/story-card-list";
 
 import data from "../../../stories.json";
 import Arrow from "@/components/arrow/arrow";
 import ImageContainer from "@/components/image-container/image-container";
+import useWindowSize from "@/customWidth";
 
 export default function Stories() {
+  const size = useWindowSize();
   const featuredStory = {
     title: "HAZY FULL MOON OF APPALACHIA",
     date: "March 2nd 2020",
@@ -16,25 +20,43 @@ export default function Stories() {
   return (
     <div className="stories">
       <div className="header">
-        <div className="featured-story">
-          <p className="sub-header">LAST MONTH'S FEATURED STORY</p>
-          <h2>{featuredStory.title}</h2>
-          <p className="date">
-            <span>{featuredStory.date}</span> by {featuredStory.author}
-          </p>
-          <p className="content">{featuredStory.content}</p>
-          <Arrow
-            text={"READ THE STORY"}
-            isFooter={false}
-            className={""}
-            style={{ color: "white" }}
-          />
-        </div>
+        {size[0] > 580 ? (
+          <div className="featured-story">
+            <p className="sub-header">LAST MONTH'S FEATURED STORY</p>
+            <h2>{featuredStory.title}</h2>
+            <p className="date">
+              <span>{featuredStory.date}</span> by {featuredStory.author}
+            </p>
+            <p className="content">{featuredStory.content}</p>
+            <Arrow
+              text={"READ THE STORY"}
+              isFooter={false}
+              className={""}
+              style={{ color: "white" }}
+            />
+          </div>
+        ) : null}
         <ImageContainer
           path="/assets/stories"
           image="moon-of-appalacia.jpg"
           alt="featured-story"
         />
+        {size[0] <= 580 ? (
+          <div className="featured-story-mobile">
+            <p className="sub-header">LAST MONTH'S FEATURED STORY</p>
+            <h2>{featuredStory.title}</h2>
+            <p className="date">
+              <span>{featuredStory.date}</span> by {featuredStory.author}
+            </p>
+            <p className="content">{featuredStory.content}</p>
+            <Arrow
+              text={"READ THE STORY"}
+              isFooter={false}
+              className={""}
+              style={{ color: "white" }}
+            />
+          </div>
+        ) : null}
       </div>
       <StoryCardList showDate={true} list={data} amount={data.length} />
     </div>
